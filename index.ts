db54e4a7bcf530d8ae7a07dce1fffbecf4984209
Carl Masak <cmasak@gmail.com>
2017-07-15 15:43:10 +0200

    Indent a little bit differently

--- index.ts
+++ index.ts
@@ -66,41 +66,36 @@ function solve <State> (puzzle: Puzzle<State>): void {
 }
 
 enum Shore { One, Other }
-
-function across(location: Shore) {
-    return location === Shore.One
-        ? Shore.Other
-        : Shore.One;
-}
+function across(location: Shore) { return location === Shore.One ? Shore.Other : Shore.One; }
 
 let puzzle = new Puzzle(
     "The boat and all the animals are on the start shore.",
     { boat: Shore.One, fox: Shore.One, goose: Shore.One, beans: Shore.One },
-    (state) => state.boat ^ 31 * (state.fox ^ 31 * (state.goose ^ 31 * state.beans)))
-    .validMove(
-        "Take the fox across.",
-        (state) => ({ ...state, boat: across(state.boat), fox: across(state.fox) }),
-        (state) => state.boat === state.fox)
-    .validMove(
-        "Take the goose across.",
-        (state) => ({ ...state, boat: across(state.boat), goose: across(state.goose) }),
-        (state) => state.boat === state.goose)
-    .validMove(
-        "Take the bag of beans across.",
-        (state) => ({ ...state, boat: across(state.boat), beans: across(state.beans) }),
-        (state) => state.boat === state.beans)
-    .validMove(
-        "Go across with an empty boat.",
-        (state) => ({ ...state, boat: across(state.boat) }))
-    .winningCondition(
-        "All the animals are now on the other side.",
-        (state) => [state.fox, state.goose, state.beans].every((l) => l === Shore.Other))
-    .losingCondition(
-        "The fox eats the goose",
-        (state) => state.boat !== state.fox && state.boat !== state.goose)
-    .losingCondition(
-        "The goose eats the beans",
-        (state) => state.boat !== state.goose && state.boat !== state.beans)
-;
+    (state) => state.boat ^ 31 * (state.fox ^ 31 * (state.goose ^ 31 * state.beans)),
+).validMove(
+    "Take the fox across.",
+    (state) => ({ ...state, boat: across(state.boat), fox: across(state.fox) }),
+    (state) => state.boat === state.fox,
+).validMove(
+    "Take the goose across.",
+    (state) => ({ ...state, boat: across(state.boat), goose: across(state.goose) }),
+    (state) => state.boat === state.goose,
+).validMove(
+    "Take the bag of beans across.",
+    (state) => ({ ...state, boat: across(state.boat), beans: across(state.beans) }),
+    (state) => state.boat === state.beans,
+).validMove(
+    "Go across with an empty boat.",
+    (state) => ({ ...state, boat: across(state.boat) }),
+).winningCondition(
+    "All the animals are now on the other side.",
+    (state) => [state.fox, state.goose, state.beans].every((l) => l === Shore.Other),
+).losingCondition(
+    "The fox eats the goose",
+    (state) => state.boat !== state.fox && state.boat !== state.goose,
+).losingCondition(
+    "The goose eats the beans",
+    (state) => state.boat !== state.goose && state.boat !== state.beans,
+);
 
 solve(puzzle);

1497db3bb1f40781550764f721c2601c02aefbdf
Carl Masak <cmasak@gmail.com>
2017-07-20 05:36:56 +0200

    Flip the two puzzles around

--- index.ts
+++ index.ts
@@ -82,44 +82,7 @@ function solve <State> (puzzle: Puzzle<State>): void {
     console.log("There's no solution.");
 }
 
-enum Shore { One, Other }
-function across(location: Shore) { return location === Shore.One ? Shore.Other : Shore.One; }
-
 let puzzle1 = new Puzzle(
-    "The boat and all the animals are on the start shore.",
-    { boat: Shore.One, fox: Shore.One, goose: Shore.One, beans: Shore.One },
-    (state) => state.boat ^ 31 * (state.fox ^ 31 * (state.goose ^ 31 * state.beans)),
-).validMove(
-    "Take the fox across.",
-    (state) => ({ boat: across(state.boat), fox: across(state.fox) }),
-    (state) => state.boat === state.fox,
-).validMove(
-    "Take the goose across.",
-    (state) => ({ boat: across(state.boat), goose: across(state.goose) }),
-    (state) => state.boat === state.goose,
-).validMove(
-    "Take the bag of beans across.",
-    (state) => ({ boat: across(state.boat), beans: across(state.beans) }),
-    (state) => state.boat === state.beans,
-).validMove(
-    "Go across with an empty boat.",
-    (state) => ({ boat: across(state.boat) }),
-).winningCondition(
-    "All the animals are now on the other side.",
-    (state) => [state.fox, state.goose, state.beans].every((l) => l === Shore.Other),
-).losingCondition(
-    "The fox eats the goose",
-    (state) => state.boat !== state.fox && state.boat !== state.goose,
-).losingCondition(
-    "The goose eats the beans",
-    (state) => state.boat !== state.goose && state.boat !== state.beans,
-);
-
-solve(puzzle1);
-
-console.log("---");
-
-let puzzle2 = new Puzzle(
     "Both vessels are empty.",
     { threeLiter: 0, fiveLiter: 0 },
     (state) => state.threeLiter ^ 31 * state.fiveLiter,
@@ -150,6 +113,43 @@ let puzzle2 = new Puzzle(
 ).winningCondition(
     "The 5 L vessel now has 4 L of water.",
     (state) => state.fiveLiter === 4,
+);
+
+solve(puzzle1);
+
+console.log("---");
+
+enum Shore { One, Other }
+function across(location: Shore) { return location === Shore.One ? Shore.Other : Shore.One; }
+
+let puzzle2 = new Puzzle(
+    "The boat and all the animals are on the start shore.",
+    { boat: Shore.One, fox: Shore.One, goose: Shore.One, beans: Shore.One },
+    (state) => state.boat ^ 31 * (state.fox ^ 31 * (state.goose ^ 31 * state.beans)),
+).validMove(
+    "Take the fox across.",
+    (state) => ({ boat: across(state.boat), fox: across(state.fox) }),
+    (state) => state.boat === state.fox,
+).validMove(
+    "Take the goose across.",
+    (state) => ({ boat: across(state.boat), goose: across(state.goose) }),
+    (state) => state.boat === state.goose,
+).validMove(
+    "Take the bag of beans across.",
+    (state) => ({ boat: across(state.boat), beans: across(state.beans) }),
+    (state) => state.boat === state.beans,
+).validMove(
+    "Go across with an empty boat.",
+    (state) => ({ boat: across(state.boat) }),
+).winningCondition(
+    "All the animals are now on the other side.",
+    (state) => [state.fox, state.goose, state.beans].every((l) => l === Shore.Other),
+).losingCondition(
+    "The fox eats the goose",
+    (state) => state.boat !== state.fox && state.boat !== state.goose,
+).losingCondition(
+    "The goose eats the beans",
+    (state) => state.boat !== state.goose && state.boat !== state.beans,
 );
 
 solve(puzzle2);

7c7591c14275b5d5b36518603fba0529fd7f3276
Carl Masak <cmasak@gmail.com>
2018-05-19 20:13:52 +0200

    Refactor Puzzle to be a POJO

--- index.ts
+++ index.ts
@@ -2,44 +2,25 @@ import * as hash from 'object-hash';
 
 declare let Set: any;
 
-declare interface ObjectConstructor {
-    assign(target: any, ...sources: any[]): any;
+interface Move<State> {
+    description: string;
+    isApplicable?: (s: State) => boolean;
+    update: (s: State) => Partial<State>;
 }
 
-class Puzzle<State> {
-    public validMoves: Array<[string, (State) => Partial<State>, (State) => boolean]> = [];
-    public winningConditions: Array<[string, (State) => boolean]> = [];
-    public losingConditions: Array<(State) => boolean> = [];
+interface Puzzle<State> {
+    initialDescription: string;
+    initialState: State;
 
-    constructor(public initialDescription: string, public initialState: State) {
-    }
+    validMoves: Array<Move<State>>;
 
-    validMove(description: string, update: (State) => Partial<State>, isApplicable: (State) => boolean = () => true): this {
-        this.validMoves.push([description, update, isApplicable]);
-        return this;
-    }
+    winningDescription: string;
+    winningCondition: (s: State) => boolean;
 
-    winningCondition(description: string, isWinning: (State) => boolean): this {
-        this.winningConditions.push([description, isWinning]);
-        return this;
-    }
-
-    losingCondition(description: string, isLosing: (State) => boolean): this {
-        this.losingConditions.push(isLosing);
-        return this;
-    }
-
-    isLosing(state: State) {
-        return this.losingConditions.some((isLosing) => isLosing(state));
-    }
+    losingConditions?: Array<(s: State) => boolean>;
 }
 
 function solve <State> (puzzle: Puzzle<State>): void {
-    if (puzzle.winningConditions.length === 0) {
-        console.log("There's no way to solve this puzzle because there are no winning conditions.");
-        return;
-    }
-
     let queue = [{ state: puzzle.initialState, trace: [puzzle.initialDescription] }];
 
     let seenSet = new Set();
@@ -51,24 +32,23 @@ function solve <State> (puzzle: Puzzle<State>): void {
             continue;
         }
 
-        for (let [description, isWinning] of puzzle.winningConditions) {
-            if (isWinning(state)) {
-                for (let step of [...trace, description]) {
-                    console.log(step);
-                }
-                return;
+        if (puzzle.winningCondition(state)) {
+            for (let step of trace) {
+                console.log(step);
             }
+            console.log(puzzle.winningDescription);
+            return;
         }
 
-        for (let [description, update, isApplicable] of puzzle.validMoves) {
-            if (isApplicable(state)) {
-                let newState = (<any>Object).assign({}, state, update(state));
-                if (seenSet.has(hash(newState)) || puzzle.isLosing(newState)) {
+        for (let move of puzzle.validMoves) {
+            if (!move.isApplicable || move.isApplicable(state)) {
+                let newState = { ...(state as any), ...(move.update(state) as any) } as State;
+                if (seenSet.has(hash(newState)) || puzzle.losingConditions && puzzle.losingConditions.some(c => c(newState))) {
                     continue;
                 }
                 queue.push({
                     state: newState,
-                    trace: [...trace, description],
+                    trace: [...trace, move.description],
                 });
             }
         }
@@ -79,73 +59,105 @@ function solve <State> (puzzle: Puzzle<State>): void {
     console.log("There's no solution.");
 }
 
-let puzzle1 = new Puzzle(
-    "Both vessels are empty.",
-    { threeLiter: 0, fiveLiter: 0 },
-).validMove(
-    "Fill up the 3 L vessel.",
-    (state) => ({ threeLiter: 3 }),
-).validMove(
-    "Fill up the 5 L vessel.",
-    (state) => ({ fiveLiter: 5 }),
-).validMove(
-    "Empty the 3 L vessel.",
-    (state) => ({ threeLiter: 0 }),
-).validMove(
-    "Empty the 5 L vessel.",
-    (state) => ({ fiveLiter: 0 }),
-).validMove(
-    "Fill the 3 L vessel from the 5 L vessel.",
-    (state) => ({
-        threeLiter: Math.min(3, state.threeLiter + state.fiveLiter),
-        fiveLiter: state.threeLiter + state.fiveLiter - Math.min(3, state.threeLiter + state.fiveLiter),
-    }),
-).validMove(
-    "Fill the 5 L vessel from the 3 L vessel.",
-    (state) => ({
-        fiveLiter: Math.min(5, state.threeLiter + state.fiveLiter),
-        threeLiter: state.threeLiter + state.fiveLiter - Math.min(5, state.threeLiter + state.fiveLiter),
-    }),
-).winningCondition(
-    "The 5 L vessel now has 4 L of water.",
-    (state) => state.fiveLiter === 4,
-);
+interface Vessels {
+    threeLiter: number;
+    fiveLiter: number;
+}
+
+let puzzle1: Puzzle<Vessels> = {
+    initialDescription: "Both vessels are empty.",
+    initialState: { threeLiter: 0, fiveLiter: 0 },
+
+    validMoves: [
+        {
+            description: "Fill up the 3 L vessel.",
+            update: () => ({ threeLiter: 3 }),
+        },
+        {
+            description: "Fill up the 5 L vessel.",
+            update: () => ({ fiveLiter: 5 }),
+        },
+        {
+            description: "Empty the 3 L vessel.",
+            update: () => ({ threeLiter: 0 }),
+        },
+        {
+            description: "Empty the 5 L vessel.",
+            update: () => ({ fiveLiter: 0 }),
+        },
+        {
+            description: "Fill the 3 L vessel from the 5 L vessel.",
+            update: (state: Vessels) => ({
+                threeLiter: Math.min(3, state.threeLiter + state.fiveLiter),
+                fiveLiter: state.threeLiter + state.fiveLiter - Math.min(3, state.threeLiter + state.fiveLiter),
+            }),
+        },
+        {
+            description: "Fill the 5 L vessel from the 3 L vessel.",
+            update: (state: Vessels) => ({
+                threeLiter: Math.min(5, state.threeLiter + state.fiveLiter),
+                fiveLiter: state.threeLiter + state.fiveLiter - Math.min(5, state.threeLiter + state.fiveLiter),
+            }),
+        },
+    ],
+
+    winningDescription: "The 5 L vessel now has 4 L of water.",
+    winningCondition: (state: Vessels) => state.fiveLiter === 4,
+};
 
 solve(puzzle1);
 
 console.log("---");
 
-enum Shore { One, Other }
-function across(location: Shore) { return location === Shore.One ? Shore.Other : Shore.One; }
+enum Shore { Start, End }
+function across(location: Shore) {
+    return location === Shore.Start
+        ? Shore.End
+        : Shore.Start;
+}
 
-let puzzle2 = new Puzzle(
-    "The boat and all the animals are on the start shore.",
-    { boat: Shore.One, fox: Shore.One, goose: Shore.One, beans: Shore.One },
-).validMove(
-    "Take the fox across.",
-    (state) => ({ boat: across(state.boat), fox: across(state.fox) }),
-    (state) => state.boat === state.fox,
-).validMove(
-    "Take the goose across.",
-    (state) => ({ boat: across(state.boat), goose: across(state.goose) }),
-    (state) => state.boat === state.goose,
-).validMove(
-    "Take the bag of beans across.",
-    (state) => ({ boat: across(state.boat), beans: across(state.beans) }),
-    (state) => state.boat === state.beans,
-).validMove(
-    "Go across with an empty boat.",
-    (state) => ({ boat: across(state.boat) }),
-).winningCondition(
-    "All the animals are now on the other side.",
-    (state) => [state.fox, state.goose, state.beans].every((l) => l === Shore.Other),
-).losingCondition(
-    "The fox eats the goose",
-    (state) => state.boat !== state.fox && state.boat !== state.goose,
-).losingCondition(
-    "The goose eats the beans",
-    (state) => state.boat !== state.goose && state.boat !== state.beans,
-);
+interface Items {
+    boat: Shore;
+    fox: Shore;
+    goose: Shore;
+    beans: Shore;
+}
+
+let puzzle2: Puzzle<Items> = {
+    initialDescription: "The boat and all the animals are on the start shore.",
+    initialState: { boat: Shore.Start, fox: Shore.Start, goose: Shore.Start, beans: Shore.Start },
+
+    validMoves: [
+        {
+            description: "Take the fox across.",
+            isApplicable: (state: Items) => state.boat === state.fox,
+            update: (state: Items) => ({boat: across(state.boat), fox: across(state.fox)}),
+        },
+        {
+            description: "Take the goose across.",
+            isApplicable: (state: Items) => state.boat === state.goose,
+            update: (state: Items) => ({boat: across(state.boat), goose: across(state.goose)}),
+        },
+        {
+            description: "Take the bag of beans across.",
+            isApplicable: (state: Items) => state.boat === state.beans,
+            update: (state: Items) => ({boat: across(state.boat), beans: across(state.beans)}),
+        },
+        {
+            description: "Go across with an empty boat.",
+            update: (state: Items) => ({boat: across(state.boat)}),
+        },
+    ],
+
+    winningDescription: "All the animals are now on the other side.",
+    winningCondition: ({ fox, goose, beans }: Items) =>
+        [fox, goose, beans].every((item) => item === Shore.End),
+
+    losingConditions: [
+        (state) => state.boat !== state.fox && state.boat !== state.goose,
+        (state) => state.boat !== state.goose && state.boat !== state.beans,
+    ],
+};
 
 solve(puzzle2);
 
@@ -156,13 +168,6 @@ function topDisk(array: number[]) {
 }
 
 type Rod = "left" | "middle" | "right";
-
-function moveDisk(from: Rod, to: Rod) {
-    return (state: { left: number[], middle: number[], right: number[] }) => ({
-        [from]: state[from].slice(0, state[from].length - 1),
-        [to]: [...state[to], topDisk(state[from])],
-    });
-}
 
 function decreasingOrder(disks: number[]) {
     return String(disks) === String(disks.slice().sort().reverse());
@@ -177,19 +182,29 @@ const MOVES: Array<{ from: Rod, to: Rod }> = [
     { from: "right", to: "middle" },
 ];
 
-let puzzle3 = new Puzzle(
-    "All the disks are on the left rod.",
-    { left: [3, 2, 1], middle: [], right: [] },
-).winningCondition(
-    "All the disks are now on the right rod.",
-    (state) => state.right.length === 3,
-).losingCondition(
-    "Wrong order of disks on a rod.",
-    (state) => Object.keys(state).some((rod) => !decreasingOrder(state[rod])),
-);
+type Hanoi = {
+    [K in Rod]: number[];
+};
 
-for (let { from, to } of MOVES) {
-    puzzle3.validMove(`Move ${from} to ${to}.`, moveDisk(from, to), (state) => state[from].length > 0);
-}
+let puzzle3: Puzzle<Hanoi> = {
+    initialDescription: "All the disks are on the left rod.",
+    initialState: { left: [3, 2, 1], middle: [], right: [] },
+
+    validMoves: MOVES.map(({ from, to }) => ({
+        description: `Move ${from} to ${to}.`,
+        isApplicable: (state: Hanoi) => state[from].length > 0,
+        update: (state: Hanoi) => ({
+            [from]: state[from].slice(0, state[from].length - 1),
+            [to]: [...state[to], topDisk(state[from])],
+        }),
+    })),
+
+    winningDescription: "All the disks are now on the right rod.",
+    winningCondition: (state: Hanoi) => state.right.length === 3,
+
+    losingConditions: [
+        (state) => Object.keys(state).some((rod: Rod) => !decreasingOrder(state[rod])),
+    ]
+};
 
 solve(puzzle3);
